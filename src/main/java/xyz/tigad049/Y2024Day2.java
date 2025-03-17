849e85f657b30dc3be5a36a673a080e78739f5cd
tigad049 <tigad049@hsd.k12.or.us>
2025-03-17 15:39:43 -0700

    finish 2024 day 2 part one

--- src/main/java/xyz/tigad049/Y2024Day2.java
+++ src/main/java/xyz/tigad049/Y2024Day2.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 
 public class Y2024Day2 {
     public static void main(String[] args) {
-        testInput();
+        partOne();
     }
 
     public static void testInput() {
         ArrayList<String> inputs;
+        Integer safeReports = 0;
 
         inputs = ReadFile.getInput("src/main/resources/2024/day2_test.txt");
         if (inputs.size() > 0) {
@@ -18,20 +19,52 @@ public class Y2024Day2 {
                 String reportString = inputs.get(i);
                 ArrayList<Integer> reportIntegers = getLevels(reportString);
                 boolean safe = isSafe(reportIntegers);
+                if (safe) {
+                    safeReports++;
+                }
 
                 System.out.println(reportString + " Safe?: " + safe);
             }
         }
+
+        System.out.println("\n" + safeReports + " safe reports!");
+    }
+
+    public static void partOne() {
+        ArrayList<String> inputs;
+        Integer safeReports = 0;
+
+        inputs = ReadFile.getInput("src/main/resources/2024/day2.txt");
+        if (inputs.size() > 0) {
+            for (int i = 0; i < inputs.size(); i++) {
+                String reportString = inputs.get(i);
+                ArrayList<Integer> reportIntegers = getLevels(reportString);
+                boolean safe = isSafe(reportIntegers);
+                if (safe) {
+                    safeReports++;
+                }
+
+                System.out.println(reportString + " Safe?: " + safe);
+            }
+        }
+
+        System.out.println("\n" + safeReports + " safe reports!");
     }
 
     private static ArrayList<Integer> getLevels(String report) {
         ArrayList<Integer> levels = new ArrayList<>();
+        Integer newNumber = 0;
 
         for (char c : report.toCharArray()) {
             if (!Character.isSpaceChar(c)) {
-                levels.add(Character.getNumericValue(c));
+                newNumber = (newNumber*10) + Character.getNumericValue(c);
+            } else {
+                levels.add(newNumber);
+                newNumber = 0;
             }
         }
+        levels.add(newNumber);
+        newNumber = 0;
 
         return levels;
     }
@@ -46,21 +79,26 @@ public class Y2024Day2 {
 
         int i = 0;
         while (i < report.size() - 1) {
-            if (Math.abs(report.get(i) - report.get((i+1))) > 3) {
+            int next = i + 1;
+            if (next > report.size()) {
+                next = report.size();
+            }
+
+            if (Math.abs(report.get(i) - report.get((next))) >= 4) {
                 isSafe = false;
             }
 
             if (isAscending) {
-                if (report.get(i) - report.get(i + 1) > 0) {
+                if (report.get(i) - report.get(next) > 0) {
                     isSafe = false;
                 }
             } else {
-                if (report.get(i) - report.get(i + 1) < 0) {
+                if (report.get(i) - report.get(next) < 0) {
                     isSafe = false;
                 }
             }
 
-            if (report.get(i) == report.get(i + 1)) {
+            if (report.get(i) == report.get(next)) {
                 isSafe = false;
             }
 

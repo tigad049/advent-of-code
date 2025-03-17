ab13493c9f4553fb788b2b59186feffe190ee1b8
tigad049 <115029111+tigad049@users.noreply.github.com>
2025-03-17 08:40:06 -0700

    restructure file directory

--- src/main/java/xyz/tigad049/Main.java
+++ src/main/java/xyz/tigad049/Main.java
@@ -3,6 +3,6 @@ package xyz.tigad049;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        DayOne2015.main(args);
+        Y2015Day1.main(args);
     }
 }
--- src/main/java/xyz/tigad049/Y2015Day1.java
+++ src/main/java/xyz/tigad049/Y2015Day1.java
@@ -2,7 +2,7 @@ package xyz.tigad049;
 
 import java.util.ArrayList;
 
-public class DayOne2015 {
+public class Y2015Day1 {
     public static void main(String[] args) {
         partOne();
         partTwo();
@@ -10,7 +10,7 @@ public class DayOne2015 {
 
     public static void testInput() {
         ArrayList<String> inputs;
-        inputs = ReadFile.getInput("src/main/resources/day1_test.txt");
+        inputs = ReadFile.getInput("src/main/resources/2015/day1_test.txt");
         if (inputs.size() > 0) {
             for (int i = 0; i < inputs.size(); i++) {
                 String plan = inputs.get(i);
@@ -32,7 +32,7 @@ public class DayOne2015 {
     public static void partOne() {
         System.out.println("Advent of Code 2015: Day 1, Part 1");
         ArrayList<String> inputs;
-        inputs = ReadFile.getInput("src/main/resources/day1.txt");
+        inputs = ReadFile.getInput("src/main/resources/2015/day1.txt");
         if (inputs.size() > 0) {
             for (int i = 0; i < inputs.size(); i++) {
                 String plan = inputs.get(i);
@@ -54,7 +54,7 @@ public class DayOne2015 {
     public static void partTwo() {
         System.out.println("Advent of Code 2015: Day 1, Part 2");
         ArrayList<String> inputs;
-        inputs = ReadFile.getInput("src/main/resources/day1.txt");
+        inputs = ReadFile.getInput("src/main/resources/2015/day1.txt");
         if (inputs.size() > 0) {
             for (int i = 1; i < inputs.size() + 1; i++) {
                 String plan = inputs.get(i - 1);
